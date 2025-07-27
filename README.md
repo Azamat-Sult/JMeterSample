b48aa96f911d27a2e41c3b001e70fc134231b3df
asultangareev <asultangareev@gmail.com>
2025-07-27 15:24:32 +0300

    application metrics export and app dashboard added

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 http://localhost:9270/metrics - метрики jmeter
 
+http://localhost:8080/actuator/prometheus - метрики приложения
+
 http://localhost:9090 - UI Prometheus
 
 http://localhost:3000 - UI Grafana (admin/admin)
--- src/main/java/ru/example/server/ServerApplication.java
+++ src/main/java/ru/example/server/ServerApplication.java
@@ -1,13 +1,27 @@
 package ru.example.server;
 
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
+    }
 
 }
